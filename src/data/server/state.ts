6614a90dd819ee7c23022401e86e99bf6956ef0a
jorelm68 <methan@umich.edu>
2024-07-29 16:28:33 -0400

    fixed warnings

--- src/data/server/state.ts
+++ src/data/server/state.ts
@@ -8,19 +8,19 @@ function useFetch<T>(type: string, id: string, initialData: T): T {
     const [data, setData] = useState<T>(initialData);
     const [refresh, setRefresh] = useState(0);
 
-    const fetchData = async (id: string) => {
-        if (!id) return;
-
-        api.general.read(type, id).then((res: Res) => {
-            if (res.success) {
-                setData(res.data);
-            }
-        })
-    };
-
     useEffect(() => {
+        const fetchData = async (id: string) => {
+            if (!id) return;
+    
+            api.general.read(type, id).then((res: Res) => {
+                if (res.success) {
+                    setData(res.data);
+                }
+            })
+        };
+
         fetchData(id);
-    }, [id, refresh]);
+    }, [type, id, refresh]);
 
     useEffect(() => {
         const interval = setInterval(() => {
