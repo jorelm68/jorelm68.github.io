9b7236fd201ea26616f8c3b757bdf840bfaf9aad
Ethan McIntyre <113143501+jorelm68@users.noreply.github.com>
2026-05-21 01:29:38 -0400

    added next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+// Replace 'your-repo-name' with the exact name of your GitHub repository
+const repoName = "jorelm68.github.io"; 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Forces Next.js to build static HTML/CSS/JS (no Node server required)
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
+  images: {
+    unoptimized: true, // Required because Next.js default Image optimization needs a live Node server
+  },
 };
 
 export default nextConfig;
